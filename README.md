42f0f09b506520b538f552fb9b5874b287b1ac1d
zche2 <zche233@gmail.com>
2026-03-04 15:32:39 -0800

    rm cached

--- README.md
+++
@@ -1,2 +0,0 @@
-# red SIF retrieval for OCI (PACE)
-try atm. transmission calculation for PACE red SIF retrieval
--- scripts/pseudo_measurement/CF/createKerne;l.jl
+++
@@ -1,6 +0,0 @@
-filename = "/Users/cfranken/data/PACE_OCI_RSRs.nc";
-pace = Dataset(filename, "r");
-
-wavlen = pace["wavelength"][:];
-RSR    = pace["RSR"];
-band   = pace["bands"];
